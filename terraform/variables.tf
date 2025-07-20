94c6a455a82277001cfda3013d84d07211789d3e
AlisinaY <yozbashi.a.y@gmail.com>
2025-07-20 09:41:49 +0200

    changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,18 +8,7 @@ variable "cluster_name" {
   default     = "my-eks-cluster"
 }
 
-
-variable "db_user" {
-  description = "Postgres DB Username"
-  type = string
-}
-
-variable "db_password" {
-  description = "Postgres DB Password"
-  type = string
-  sensitive = true
-}
-
+# --- Database ---
 variable "db_host" {
   description = "Postgres DB Host"
   type        = string
@@ -41,6 +30,7 @@ variable "db_password" {
   sensitive   = true
 }
 
+# --- OpenAI ---
 variable "openai_api_key" {
   description = "OpenAI API Key"
   type        = string
