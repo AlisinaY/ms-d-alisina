3f08ad053b44ba28b3ee04bd656d2243d9155943
AlisinaY <yozbashi.a.y@gmail.com>
2025-07-27 16:29:25 +0200

    changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,7 @@ variable "cluster_name" {
 variable "db_host" {
   description = "Postgres DB Host"
   type        = string
+  # wird glöscht
   default = "shopping-assistant-db.czcmm4esc0mr.eu-central-1.rds.amazonaws.com"
 }
 
@@ -24,6 +25,7 @@ variable "db_name" {
 variable "db_user" {
   description = "Postgres DB Username"
   type        = string
+  # wird gelöscht
   default = "postgres"
 }
 
@@ -31,6 +33,7 @@ variable "db_password" {
   description = "Postgres DB Password"
   type        = string
   sensitive   = true
+  # default wird gelöscht, wenn das Projekt zu Ende ist.
   default = "MySecurePass123!"
 }
 
@@ -39,5 +42,6 @@ variable "openai_api_key" {
   description = "OpenAI API Key"
   type        = string
   sensitive   = true
+  # wird gelöscht 
   default = "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"
 }
