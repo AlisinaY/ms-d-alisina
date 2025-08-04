68daea7e01e0d29402a9e565b17aad16a749cc6b
AlisinaY <yozbashi.a.y@gmail.com>
2025-08-04 13:35:31 +0200

    changed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_endpoint" {
-  value = aws_eks_cluster.my_eks.endpoint
+  value = aws_eks_cluster.msdemo_dev_eks.endpoint
 }
 
 output "cluster_name" {
-  value = aws_eks_cluster.my_eks.name
+  value = aws_eks_cluster.msdemo_dev_eks.name
 }
 
 output "kubeconfig_certificate_authority_data" {
-  value = aws_eks_cluster.my_eks.certificate_authority[0].data
+  value = aws_eks_cluster.msdemo_dev_eks.certificate_authority[0].data
 }
